1e8c2e49ab3afd3c4962b5ea1714a31e982e6e38
Paul K. Korir <31738053+paulkorir@users.noreply.github.com>
2021-05-26 13:54:21 +0100

    Notes and score

--- main.py
+++ main.py
@@ -2,6 +2,36 @@ import os
 import sys
 import math
 
+"""
+Score: 1 full point. Well done!
+Notes:
+-------
+First of all, your work is correct. I noticed you forked instead of cloning. It's actually
+more efficient to clone though there is something to learn from cloning. 
+
+Docstrings: 
+- typically, we would write the title on the first line immediately after the first triple quote. By skipping to the next line
+we inadvertently add a newline, which will show up in the parsed documentation. 
+- There are formal formats that one could follow. These can be parsed by, for example, PyCharm so that when you click the function
+name at its point of use and press F1 you will see a bubble with the functions usage - very handy when you have functions in 
+different modules. 
+- One of the most important tools for documentation is Sphinx, primarily built to handle documentation of Python
+objects (see https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#python-signatures). Sphinx can use any 
+markup language (Markdown, reStructureText) which it can then convert in HTML, LaTeX etc. Here is how we can improve the docstring to 
+use reStructuredText by placing some delimiters on the variables (it uses info field lists defined at 
+https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#info-field-lists):
+
+    :param float a: coefficient of x^2
+    :param float b: coefficient of x
+    :param float c: constant
+    :return: both solutions
+    :rtype: tuple(float, float)
+    
+- You correctly identifed that we were missing brackets for each solution. 
+Another solution is using / for each dividend i.e. (-b + math.sqrt(discriminant)) / 2 / a
+
+- From next class I'll score each exercise with 10 points for a max of 100 points so we can have some granularity. 
+"""
 
 def calculate(a, b, c):
     """
@@ -16,6 +46,7 @@ def calculate(a, b, c):
         float:Returning value
 
        """
+    
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         return None, None
