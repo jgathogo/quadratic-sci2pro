ee432a3c60421377f798eb240582d894f1e2706f
James Gathogo <james@gathogo.co.ke>
2021-05-26 11:26:56 +0300

    Fix bug in calculate function

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def calculate(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         return None, None
-    x1 = (-b + math.sqrt(discriminant)) / 2 * a
-    x2 = (-b - math.sqrt(discriminant)) / 2 * a
+    x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    x2 = (-b - math.sqrt(discriminant)) / (2 * a)
     return x1, x2
 
 
